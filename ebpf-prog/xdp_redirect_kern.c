b3468423aab9f2a57f5c9e16a655fd504fcd3f60
Huang, Guanxu <guanxu.huang@intel.com>
2023-01-28 17:21:58 +0800

    add ebpf program for encryption side

---
+++ ebpf-prog/xdp_redirect_kern.c
@@ -0,0 +1,34 @@
+#include <linux/bpf.h>
+#include <linux/types.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <bpf/bpf_helper_defs.h>
+#include <bpf/bpf_endian.h>
+
+#define SEC(NAME) __attribute__((section(NAME), used))
+static unsigned REDIRECT_IFACE_IDX = 12;
+
+SEC("xdp_redirect")
+int xdp_redirect_func(struct xdp_md *ctx)
+{
+        void *data_end = (void *)(long)ctx->data_end;
+        void *data = (void *)(long)ctx->data;
+        struct ethhdr *eth_hdr = data;
+        unsigned int ethhdr_off = sizeof(*eth_hdr);
+        struct iphdr *ip_hdr = data + ethhdr_off;
+        unsigned int iphdr_off = sizeof(*ip_hdr);
+
+        if (data + ethhdr_off + iphdr_off > data_end) {
+                return XDP_DROP;
+        }
+
+        if ((eth_hdr->h_proto != bpf_htons(ETH_P_IP)) || (ip_hdr->protocol != 0x11)) {
+                return XDP_PASS;
+        } else {
+                bpf_xdp_adjust_head(ctx, ethhdr_off + iphdr_off);
+        }
+
+        return bpf_redirect(REDIRECT_IFACE_IDX, 0);
+}
+
+char _license[] SEC("license") = "GPL";
